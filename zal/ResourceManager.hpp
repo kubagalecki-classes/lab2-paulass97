4da0740e1086069b390a75bbc28511c3db4ea11c
paulass97 <73600661+paulass97@users.noreply.github.com>
2020-11-10 23:11:45 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,69 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <iostream>
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+
+  ResourceManager() {
+    r = new Resource;
+  }
+
+    ResourceManager(const ResourceManager& resM)
+    {
+        r = new Resource{*resM.r};
+        std::cout << "konstruktor kopiujący: " << r << std::endl;
+    }
+    
+    ResourceManager(ResourceManager&& resM) 
+    {
+        std::cout << "konstruktor przenoszacy: " << std::endl;
+        r    = move(resM.r); 
+        resM.r = nullptr;
+    }
+   
+    ResourceManager& operator=(const ResourceManager& resM)
+    {
+        if (!(r == nullptr)) {
+            std::cout << "samo usuwanie: " << std::endl;
+            delete r;
+        }
+        r = new Resource{*resM.r};
+        std::cout << "Operator kopiujacy: " << r << std::endl;
+        return *this;
+    }
+
+    ResourceManager& operator=(ResourceManager&& resM)
+    {
+        if (&resM == this)
+        {
+            std::cout << "samo przenoszenie: " << std::endl;
+            return *this;
+        }
+        else if (!(r == nullptr)) {
+            std::cout << "samo usuwanie" << std::endl;
+            delete r;
+        }
+        std::cout << "Operator przenoszacy: " << std::endl;
+        r = move(resM.r); 
+
+        resM.r = nullptr;
+        return *this;
+    }
+
+    ~ResourceManager()
+    {
+        std::cout << "Destruktor: " << r << std::endl;
+        delete r;
+    }
+
+  double get()
+  {
+    return r->get();
+  }
+
+private:
+    Resource* r = nullptr;
 };
