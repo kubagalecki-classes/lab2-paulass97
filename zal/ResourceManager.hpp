c366ae329a2d880ba0c7412b311e6491992de43e
paulass97 <73600661+paulass97@users.noreply.github.com>
2020-11-10 23:32:29 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -7,45 +7,44 @@ using namespace std;
 class ResourceManager
 {
 public:
-
   ResourceManager() {
     res = new Resource;
   }
 
-    ResourceManager(const ResourceManager& rs) 
+    ResourceManager(const ResourceManager& r) 
     {
-        res = new Resource{*rs.res};
+        res = new Resource{*r.res};
         cout << res<<endl;
     }
 
-    ResourceManager& operator=(const ResourceManager& rs)
+    ResourceManager& operator=(const ResourceManager& r)
     {
         if (!(res == nullptr)) {
             delete res;
         }
-        res = new Resource{*rs.res};
+        res = new Resource{*r.res};
         cout <<  res << endl;
         return *this;
     }
 
-    ResourceManager(ResourceManager&& rs)
+    ResourceManager(ResourceManager&& r)
     {
-        res    = move(rs.res);
-        rs.res = nullptr;
+        res    = move(r.res);
+        r.res = nullptr;
     }
 
-    ResourceManager& operator=(ResourceManager&& rs)
+    ResourceManager& operator=(ResourceManager&& r)
     {
-        if (&rs == this)
+        if (&r == this)
         {
             return *this;
         }
         else if (!(res == nullptr)) {
             delete res;
         }
-        res = move(rs.res); 
+        res = move(r.res); 
 
-        rs.res = nullptr;
+        r.res = nullptr;
         return *this;
     }
 
